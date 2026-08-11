632a96913c63f8593dea8e8c7d8d5d4f575cce20
remusvi <adafax4@gmail.com>
2026-08-11 12:55:22 -0400

    sub proc spawning correctly

--- core/servers/start_server.cpp
+++ core/servers/start_server.cpp
@@ -12,15 +12,15 @@ void start_server(std::string& server_id) {
     std::string resource_path = get_resource_path();
     fs::path resource_dir(resource_path);
     fs::path server_dir = resource_dir/ "servers"/ server_id;
-    fs::path binary_dir = resource_dir / "servers" / server_id / "/Users/adafaralph/dev/Beacon2.0/Resources/servers/56eb1aac-eb5c-497e-8843-355264cbdedd/server.jar";
+    fs::path binary_dir = resource_dir / "servers" / server_id / "server.jar";
 
     // Spawn server.jar subprocess
     bp::child c(
             bp::search_path("java"),
             "-jar",
-            binary_dir.string(),
-            bp::start_dir(server_dir.string())
-        );
+            "/Users/adafaralph/dev/Beacon2.0/Resources/servers/56eb1aac-eb5c-497e-8843-355264cbdedd/server.jar",
+            bp::start_dir("/Users/adafaralph/dev/Beacon2.0/Resources/servers/56eb1aac-eb5c-497e-8843-355264cbdedd")
+    );
 
     c.wait();
 }
